b150985dd871cf0dc0012210f87389dded80f448
Ayanbola <olaoluwaayanbola@gmail.com>
2022-08-04 22:18:13 +0100

    task implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import InputBox from './Components/Inputbox/InputBox';
+import { useState } from 'react';
 import './App.css'
-const App:React.FC = () => {
+
+const App: React.FC = () => {
+  const [Todo, setTodo] = useState<string>("");
+  console.log(Todo)
   return (
     <div className="App">
       <div className="app-container">
         <div className="header">
-          <h2>TASK.ORG</h2>
+          <h1>TASK.ORG</h1>
         </div>
+        <InputBox Todo={Todo} setTodo={setTodo}></InputBox>
       </div>
     </div>
   );
