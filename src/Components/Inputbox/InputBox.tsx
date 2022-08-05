2b8dd688e0c1690573db32489e11f7bd2b724fda
Ayanbola <olaoluwaayanbola@gmail.com>
2022-08-05 09:56:43 +0100

    styled-input box

--- src/Components/Inputbox/InputBox.tsx
+++ src/Components/Inputbox/InputBox.tsx
@@ -4,15 +4,19 @@ interface props {
     Todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
 }
+const handleSubmit = (event:any) => {
+    event.preventDefault();
+  }
 const InputBox: React.FC<props> = ({ Todo, setTodo }) => {
     return (
         <div className='InputBox_container'>
-            <form action="" className='InputBox_form'>
+            <form action="" className='InputBox_form' onSubmit={handleSubmit}>
                 <input
                     type="text"
                     className='input_sec'
                     value={Todo}
                     onChange={(e) => setTodo(e.target.value)}
+                    
                 />
                 <button className='button'>Go</button>
             </form>
